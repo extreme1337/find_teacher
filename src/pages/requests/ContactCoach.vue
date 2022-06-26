d1d4981dc16c7eb4abbe0909c6488149ef3e56d4
Marko Miseljic <marko.miseljic.14@gmail.com>
2022-06-26 20:58:45 +0200

    ReqeustItem ContactCoach

--- src/pages/requests/ContactCoach.vue
+++ src/pages/requests/ContactCoach.vue
@@ -1,19 +1,47 @@
 <template>
-    <form>
+    <form @submit.prevent="submitForm">
         <div class="form-control">
             <label for="email">Your E-Mail</label>
-            <input type="email" id="email"/>
+            <input type="email" id="email" v-model.trim="email"/>
         </div>
         <div class="form-control">
             <label for="message">Message</label>
-            <textarea rows="5" id="message"></textarea>
+            <textarea rows="5" id="message" v-model.trim="message"></textarea>
         </div>
+        <p class="errors" v-if="!formIsValid">Please enter a valid email and no-empty message.</p>
         <div class="actions">
             <base-button>Send Message</base-button>
         </div>
     </form>
 </template>
 
+<script>
+export default {
+    data(){
+        return {
+            email: '',
+            message: '',
+            formIsValid: true,
+        }
+    },
+    methods: {
+        submitForm(){
+            this.formIsValid = true;
+            if(this.email === '' || !this.email.includes('@') || this.message ===''){
+                this.formIsValid = false;
+                return;
+            }
+            this.$store.dispatch('requests/contactCoach', {
+                email: this.email,
+                message: this.message,
+                coachId: this.$route.params.id
+            });
+            this.$router.replace('/coaches');
+
+        }
+    }
+}
+</script>
 
 
 <style scoped>
